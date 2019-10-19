5153c3b1c324ce0f8a7679f6e652edac8590e027
Daniel Engbert <end1@umbc.edu>
2019-10-19 14:23:50 -0400

    committing misc messy changes to process_config.sh before cleaning it perfectly to work.

--- process_config.sh
+++ process_config.sh
@@ -3,6 +3,7 @@
 # and convert each file one by one to dnxhd
 # and rotate files (where rot!=""), adding black padding as needed
 # (the point is someone can manually modify the file before running this step)
+# mediainfo <video> # (useful command)
 
 
 # https://en.wikipedia.org/wiki/List_of_Avid_DNxHD_resolutions
@@ -21,11 +22,13 @@ CONV_FLAGS=(
     #-af 'aresample=async=1024'
     # ^i don't think we need the async flag until we start combining videos
 LOG_FILE="tmp-ffmpeg-log.txt"
+OUT_LIST="tmp-combine-list.txt"
+OUT_COMBINED="out-combined.mov"
 
 function process_config() {
     CONFIG_FILE="$1"
     rm -rf /tmp/converted_vids && mkdir -p /tmp/converted_vids
-    rm -f $LOG_FILE
+    rm -f $LOG_FILE $OUT_LIST $OUT_COMBINED
 
     if [ "$#" -ne 1 ]; then
         echo "ERROR expected 1 arg, received: $#"
@@ -53,23 +56,47 @@ function process_config() {
         fi
         fname=${ARR[0]}; rot=${ARR[1]}; width=${ARR[2]}; height=${ARR[3]}
 
+
         # set flags for rotation (if needed):
         # https://stackoverflow.com/a/9570992
         # TODO: delete this part?
         flags_rot=() # empty array
+        str=""
         if [ "$rot" == "90" ]; then
+            str="transpose=1"
             flags_rot+=(-vf 'transpose=1')
         elif [ "$rot" == "180" ]; then
+            str="transpose=2,transpose=2"
             flags_rot+=(-vf 'transpose=2,transpose=2')
         elif [ "$rot" == "270" ]; then
+            str="transpose=2"
             flags_rot+=(-vf 'transpose=2')
         fi
 
         # rotate and add padding to video if needed (modify -vf filter)
+        # TODO: note: also must check if dimensions are 1080x1920 (for iphone)
         if ! [ -z "$rot" ]; then
-            flags[$loc]="${flags[$loc]},pad=1920:1080:ih:(ow-iw)/2:color=AliceBlue"
+            # TODO: don't hardcode  width, height?
+            flags[$loc]="${flags[$loc]},pad=width=1920:height=1080:x=ih:y=(ow-iw)/2:color=AliceBlue,transpose=1" #,$str
             echo "  modified -vf filter to: \"${flags[$loc]}\""
         fi
+
+        # TODO: TODO: TODO: TODO:
+        # TODO: magic line for converting all vertical videos to horizontal with padding:
+        # !!!!!!!!
+        #ffmpeg -hide_banner -y -i iphone/VER_2.MOV -c:v dnxhd -vf "scale=-1:1080,pad=1920:1080:(ow-iw)/2:color=AliceBlue,fps=30000/1001,format=yuv422p" -b:v 45M -c:a pcm_s16le /tmp/converted_vids/iphone_ver2.mov
+        # !!!!!!!!
+        # links:
+        # filtergraph defined: http://ffmpeg.org/ffmpeg-filters.html#Filtergraph-description
+        # read definition/options for each filter to understand
+        # https://ffmpeg.org/ffmpeg-filters.html#scale-1
+        # https://ffmpeg.org/ffmpeg-filters.html#pad-1
+        # TODO: TODO: TODO: TODO:
+
+
+
+
+
 
         # generate filename
         # TODO: maybe use existing path/filename but prepend "CONV--"
@@ -89,14 +116,18 @@ function process_config() {
         # TODO: use another tool/command to copy the metadata afterwards? (map_metadata?)
         #touch -r "$fname" "$newfile"  # copy original file's metadata
 
-        ## rotate video if needed
-        #if ! [ -z "$flags_rot" ]; then
-        #    # TODO: now do rotation
-        #    #   https://video.stackexchange.com/a/17699
-        #    #   see my MTS conversion script too
-        #fi
-
+        echo "file $newfile" >> $OUT_LIST
     done < "$CONFIG_FILE"
+
+    echo "" && echo "list to combine outputted to $OUT_LIST"
+    echo "combining videos..." && echo ""
+    ffmpeg -f concat -y -safe 0 -i $OUT_LIST -c copy $OUT_COMBINED </dev/null >>${LOG_FILE} 2>&1
+
+    if [ "$?" -ne "0" ]; then
+        echo "ERROR: (exit code $?) combining videos: $fname"
+        exit 1
+    fi
+    echo "combined video generated: $OUT_COMBINED"
 }
 
 # returns the location of the value corresponding to the -vf flag in CONV_FLAGS
